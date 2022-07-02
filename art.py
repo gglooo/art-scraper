f24855cdadd81c82118ebb0b7584dd65ca89b388
gglooo <84050160+gglooo@users.noreply.github.com>
2022-07-02 18:48:13 +0200

    first working version

--- art.py
+++ art.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-import os
-import unidecode
 from artist import Artist
+from requests_html import HTMLSession
+import re
+import random
 
 from typing import Dict, List, Tuple
 
@@ -10,13 +11,16 @@ AUTHORS = "https://www.wga.hu/cgi-bin/artist.cgi?"
 
 def parse_parameters(parameters: Dict[str, str],
                      kwargs: Dict[str, str]) -> None:
-
+    changed = False
     for key in kwargs.keys():
+        if key == "number":
+            key = "from"
+            changed = True
         if key not in parameters and key.capitalize() not in parameters:
             raise KeyError(f"{key} is not a valid parameter")
         
         if key in parameters:
-            parameters[key] = kwargs[key]
+            parameters[key] = kwargs[key if not changed else "number"]
         else:
             parameters[key.capitalize()] = kwargs[key]
     
@@ -30,7 +34,7 @@ def get_url(parameters: Dict[str, str]) -> str:
     return url[:-1] # returning without last '&' symbol
 
 
-def parse_artist(element) -> Tuple[str, str]:
+def parse_artist(element) -> Artist:
     name = element.find("a").text
     url = element.find("a").get("href")
     art = Artist(name, url)
@@ -41,26 +45,34 @@ def parse_artist(element) -> Tuple[str, str]:
 def get_image(**kwargs):
     parameters = {"Profession": "any", "School": "any",
                   "Period": "any", "Time-line": "any",
-                  "from": "3", "max": "50", "Sort": "Name",
+                  "from": "0", "max": "6000", "Sort": "Name",
                   "letter": "-", "width": "700", "targetleft": "0"}
 
     parse_parameters(parameters, kwargs)
     url = get_url(parameters)
     print(url)
     
-    response = requests.get(url)
+    session = HTMLSession()
+    response = session.get(url)
     response.encoding = "utf-8"
 
     soup = BeautifulSoup(response.text, "html.parser")
 
     element = soup.find("td", class_="ARTISTLIST")
-    print(element)
     if not element:
         print("No artwork found")
         return -1
+
     artist = parse_artist(element)
-    print(artist.url_name)
+
+    response = session.get(artist.url)
+    response.encoding = "utf-8"
+
+    all_pictures_url = re.findall(r"<img src=\"/preview/(.*?)\"", response.text)
+    displayed_pic = random.choice(all_pictures_url)
+
+    print(f"{Artist.ART_URL}{displayed_pic}")
 
 
 if __name__ == "__main__":
-    get_image(school="Hungarian", period="Impressionism")
+    get_image(number=str(random.randint(0, 4000)))
