8e8a28d91f3ff1e35ae26b2f86af8624b1c767c8
gglooo <84050160+gglooo@users.noreply.github.com>
2022-07-02 18:49:48 +0200

    first working version

---
+++ src/artist.py
@@ -0,0 +1,23 @@
+from unidecode import unidecode
+
+class Artist():
+
+    ART_URL = "https://www.wga.hu/art/"
+
+    def __init__(self, name: str, url: str):
+        names = name.split(", ")
+
+        if len(names) < 2:
+            raise ValueError("Invalid artist name")
+
+        self.surname = names[0].lower()
+        self.other_names = ", ".join(names[1:])
+        self.url = url
+        self.url_name = self.get_url_name()
+        self.artist_url = Artist.ART_URL + self.url_name[0] + "/" + self.url_name
+    
+    def __str__(self):
+        return self.surname.capitalize() + ", " + self.other_names
+    
+    def get_url_name(self):
+        return unidecode(self.surname.split(" ")[0])[:8]
